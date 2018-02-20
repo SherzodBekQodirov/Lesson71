060090ce2e1735cd4ada77d62812b7a663e8b1b3
Sher Kodirov <sher@Shers-MacBook-Pro.local>
2018-02-20 19:38:12 +0500

    Initial commit

---
+++ app/src/main/java/ru/startandroid/lesson71/PrefActivity.java
@@ -0,0 +1,12 @@
+package ru.startandroid.lesson71;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PrefActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref);
+    }
+}
